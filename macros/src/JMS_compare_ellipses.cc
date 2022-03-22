2263dd6673bf51a3a7d86c8f71b02a5268c1ceca
apaasch <alexanderpaa94@gmail.com>
2022-03-22 11:54:22 +0100

    new comparisons

--- macros/src/JMS_compare_ellipses.cc
+++ macros/src/JMS_compare_ellipses.cc
@@ -13,19 +13,22 @@
 // #include <io.h>
 using namespace std;
 
-inline void draw(vector<TGraph*> graph, vector<TEllipse*> ellipse, vector<int> color,  vector<TString> names, TString name);
+void draw(vector<TGraph*> graph, vector<TEllipse*> ellipse, vector<int> color, vector<TString> names, TString name);
+void CompareAreas(vector<TEllipse*> ellipse, vector<TString> names);
+MapVD ExtractFromFile(TString path, TString c);
+VecD ExtractPoints(TString line, const TString jms);
+TEllipse* build_ellipse(MapVD points, TString name="");
+TGraph* graph_multiple_points(MapVD points);
 inline void set_ellipse(TEllipse *ellipse, int color);
 inline void set_graph(TGraph *graph, int color);
-MapVD ExtractFromFile(TString path, TString c);
 inline double angle_to_xaxis(const VecD vec);
 inline double distance_points(const VecD vec1, const VecD vec2);
 inline double length_vector(const VecD vec);
-inline VecD ExtractPoints(TString line, const TString jms);
 inline VecD subtract_two_vectors(const VecD vec1, const VecD vec2);
-inline TEllipse* build_ellipse(MapVD points);
-inline TGraph* graph_multiple_points(MapVD points);
 
 bool debug = false;
+
+vector<double> lumis = {138., 36., 41., 60.};
 
 int main(int argc, char* argv[]){
 
@@ -40,12 +43,25 @@ int main(int argc, char* argv[]){
   MapVDD JMSfactor;
 
   cout << "\nExtract Points ... " << endl;
+  JMSfactor["combine_uncor"]        = ExtractFromFile(save_path+"/JetCorrections/fit/combine/combine/100/BinWidth_1/JMS.txt", "combine");
+  JMSfactor["combine_cor"]          = ExtractFromFile(save_path+"/JetCorrections/fit/combine/combine/100/BinWidth_1/JMScorrelated.txt", "combine");
+  JMSfactor["combine_lin_cor"]      = ExtractFromFile(save_path+"/JetCorrections/fit/combine/combine/100/BinWidth_1/lin/JMScorrelated.txt", "combine");
+  JMSfactor["combine_cor_noBKGsys"] = ExtractFromFile(save_path+"/JetCorrections/fit/combine/combine/100/BinWidth_1/noBKGsys/JMScorrelated.txt", "elec");
+
   JMSfactor["combine_add"] = ExtractFromFile(save_path+"/JetCorrections/fit_t+b/combine/combine/100/BinWidth_1/JMS.txt", "combine");
-  JMSfactor["muon_add"] = ExtractFromFile(save_path+"/JetCorrections/fit_t+b/combine/muon/100/BinWidth_1/JMS.txt", "muon");
-  JMSfactor["elec_add"] = ExtractFromFile(save_path+"/JetCorrections/fit_t+b/combine/elec/100/BinWidth_1/JMS.txt", "elec");
+  JMSfactor["muon_add"]    = ExtractFromFile(save_path+"/JetCorrections/fit_t+b/combine/muon/100/BinWidth_1/JMS.txt", "muon");
+  JMSfactor["elec_add"]    = ExtractFromFile(save_path+"/JetCorrections/fit_t+b/combine/elec/100/BinWidth_1/JMS.txt", "elec");
+
   JMSfactor["combine_V20"] = ExtractFromFile(save_path+"/JetCorrections/fit_V20/combine/combine/100/BinWidth_1/JMS.txt", "elec");
-  JMSfactor["muon_V20"] = ExtractFromFile(save_path+"/JetCorrections/fit_V20/combine/muon/100/BinWidth_1/JMS.txt", "elec");
-  JMSfactor["elec_V20"] = ExtractFromFile(save_path+"/JetCorrections/fit_V20/combine/elec/100/BinWidth_1/JMS.txt", "elec");
+  JMSfactor["muon_V20"]    = ExtractFromFile(save_path+"/JetCorrections/fit_V20/combine/muon/100/BinWidth_1/JMS.txt", "elec");
+  JMSfactor["elec_V20"]    = ExtractFromFile(save_path+"/JetCorrections/fit_V20/combine/elec/100/BinWidth_1/JMS.txt", "elec");
+
+  JMSfactor["2016_combine"]     = ExtractFromFile(save_path+"/JetCorrections/fit/2016/combine/40/BinWidth_1/JMS.txt", "combine");
+  JMSfactor["2017_combine"]     = ExtractFromFile(save_path+"/JetCorrections/fit/2017/combine/40/BinWidth_1/JMS.txt", "combine");
+  JMSfactor["2018_combine"]     = ExtractFromFile(save_path+"/JetCorrections/fit/2018/combine/40/BinWidth_1/JMS.txt", "combine");
+  JMSfactor["2016_combine_cor"] = ExtractFromFile(save_path+"/JetCorrections/fit/2016/combine/40/BinWidth_1/lin/JMScorrelated.txt", "combine");
+  JMSfactor["2017_combine_cor"] = ExtractFromFile(save_path+"/JetCorrections/fit/2017/combine/40/BinWidth_1/lin/JMScorrelated.txt", "combine");
+  JMSfactor["2018_combine_cor"] = ExtractFromFile(save_path+"/JetCorrections/fit/2018/combine/40/BinWidth_1/lin/JMScorrelated.txt", "combine");
 
   for(TString c: channels){
     if(debug) cout << "\t ... channel "+c << endl;
@@ -55,26 +71,50 @@ int main(int argc, char* argv[]){
 
   // Fill Graph and Ellipse ------------------------------------------------------------------------
   cout << "Create Graphs ... " << endl;
-  TGraph* extrema_combine = graph_multiple_points(JMSfactor["combine"]);
-  TGraph* extrema_muon = graph_multiple_points(JMSfactor["muon"]);
-  TGraph* extrema_elec = graph_multiple_points(JMSfactor["elec"]);
+  TGraph* extrema_combine              = graph_multiple_points(JMSfactor["combine"]);
+  TGraph* extrema_combine_cor          = graph_multiple_points(JMSfactor["combine_cor"]);
+  TGraph* extrema_combine_lin_cor      = graph_multiple_points(JMSfactor["combine_lin_cor"]);
+  TGraph* extrema_combine_cor_noBKGsys = graph_multiple_points(JMSfactor["combine_cor_noBKGsys"]);
+
+  TGraph* extrema_muon     = graph_multiple_points(JMSfactor["muon"]);
+  TGraph* extrema_elec     = graph_multiple_points(JMSfactor["elec"]);
   TGraph* extrema_add_comb = graph_multiple_points(JMSfactor["combine_add"]);
   TGraph* extrema_add_muon = graph_multiple_points(JMSfactor["muon_add"]);
   TGraph* extrema_add_elec = graph_multiple_points(JMSfactor["elec_add"]);
+
   TGraph* extrema_V20_comb = graph_multiple_points(JMSfactor["combine_V20"]);
   TGraph* extrema_V20_muon = graph_multiple_points(JMSfactor["muon_V20"]);
   TGraph* extrema_V20_elec = graph_multiple_points(JMSfactor["elec_V20"]);
 
+  TGraph* extrema_2016_comb     = graph_multiple_points(JMSfactor["2016_combine"]);
+  TGraph* extrema_2017_comb     = graph_multiple_points(JMSfactor["2017_combine"]);
+  TGraph* extrema_2018_comb     = graph_multiple_points(JMSfactor["2018_combine"]);
+  TGraph* extrema_2016_comb_cor = graph_multiple_points(JMSfactor["2016_combine_cor"]);
+  TGraph* extrema_2017_comb_cor = graph_multiple_points(JMSfactor["2017_combine_cor"]);
+  TGraph* extrema_2018_comb_cor = graph_multiple_points(JMSfactor["2018_combine_cor"]);
+
   cout << "Create Ellipses ... " << endl;
-  TEllipse* ellipse_combine = build_ellipse(JMSfactor["combine"]);
-  TEllipse* ellipse_muon = build_ellipse(JMSfactor["muon"]);
-  TEllipse* ellipse_elec = build_ellipse(JMSfactor["elec"]);
+  TEllipse* ellipse_combine              = build_ellipse(JMSfactor["combine"]);
+  TEllipse* ellipse_combine_cor          = build_ellipse(JMSfactor["combine_cor"]);
+  TEllipse* ellipse_combine_lin_cor      = build_ellipse(JMSfactor["combine_lin_cor"]);
+  TEllipse* ellipse_combine_cor_noBKGsys = build_ellipse(JMSfactor["combine_cor_noBKGsys"]);
+
+  TEllipse* ellipse_muon     = build_ellipse(JMSfactor["muon"]);
+  TEllipse* ellipse_elec     = build_ellipse(JMSfactor["elec"]);
   TEllipse* ellipse_add_comb = build_ellipse(JMSfactor["combine_add"]);
   TEllipse* ellipse_add_muon = build_ellipse(JMSfactor["muon_add"]);
   TEllipse* ellipse_add_elec = build_ellipse(JMSfactor["elec_add"]);
+
   TEllipse* ellipse_V20_comb = build_ellipse(JMSfactor["combine_V20"]);
   TEllipse* ellipse_V20_muon = build_ellipse(JMSfactor["muon_V20"]);
   TEllipse* ellipse_V20_elec = build_ellipse(JMSfactor["elec_V20"]);
+
+  TEllipse* ellipse_2016_comb     = build_ellipse(JMSfactor["2016_combine"]);
+  TEllipse* ellipse_2017_comb     = build_ellipse(JMSfactor["2017_combine"]);
+  TEllipse* ellipse_2018_comb     = build_ellipse(JMSfactor["2018_combine"]);
+  TEllipse* ellipse_2016_comb_cor = build_ellipse(JMSfactor["2016_combine_cor"]);
+  TEllipse* ellipse_2017_comb_cor = build_ellipse(JMSfactor["2017_combine_cor"]);
+  TEllipse* ellipse_2018_comb_cor = build_ellipse(JMSfactor["2018_combine_cor"]);
 
   // Start drawing ---------------------------------------------------------------------------------
   cout << "Start plotting ... " << endl;
@@ -84,10 +124,40 @@ int main(int argc, char* argv[]){
   gStyle->SetLegendBorderSize(0);
 
   // void draw(vector<TGraph*> graph, vector<TEllipse*> ellipse, vector<int> color,  vector<TString> leg, TString name)
-  draw({extrema_combine, extrema_muon, extrema_elec}, {ellipse_combine, ellipse_muon, ellipse_elec}, {kBlue+2, kRed+2, kGreen+2}, {"combine", "muon", "elec"}, "channel_comparison");
-  draw({extrema_combine, extrema_add_comb}, {ellipse_combine, ellipse_add_comb}, {kBlue+2, kRed+2}, {"data-bkg", "tt+bkg"}, "bkg_comparison");
-  draw({extrema_combine, extrema_V20_comb}, {ellipse_combine, ellipse_V20_comb}, {kBlue+2, kRed+2}, {"JEC V11", "JEC V20"}, "JECversion_comparison");
-  draw({extrema_add_comb, extrema_add_muon, extrema_add_elec}, {ellipse_add_comb, ellipse_add_muon, ellipse_add_elec}, {kBlue+2, kRed+2, kGreen+2}, {"combine", "muon", "elec"}, "AddBKGChannelComparison");
+  // draw({extrema_combine, extrema_combine_cor}, {ellipse_combine, ellipse_combine_cor}, {kBlue+2, kRed+2}, {"uncorrelated", "correlated"}, "correlation_comparison");
+  // draw({extrema_combine_cor, extrema_combine_cor_noBKGsys}, {ellipse_combine_cor, ellipse_combine_cor_noBKGsys}, {kBlue+2, kRed+2}, {"with bkg rate", "no bkg rate"}, "BKGsys_cor_comparison");
+  // draw({extrema_combine_cor, extrema_2016_comb, extrema_2017_comb, extrema_2018_comb}, {ellipse_combine_cor, ellipse_2016_comb, ellipse_2017_comb, ellipse_2018_comb}, {kBlack, kBlue+2, kRed+2, kGreen+2}, {"Combine", "2016", "2017", "2018"}, "Year_combine_comparison");
+  draw({extrema_combine_lin_cor, extrema_2016_comb_cor, extrema_2017_comb_cor, extrema_2018_comb_cor}, {ellipse_combine_lin_cor, ellipse_2016_comb_cor, ellipse_2017_comb_cor, ellipse_2018_comb_cor}, {kBlack, kBlue+2, kRed+2, kGreen+2}, {"Combine", "2016", "2017", "2018"}, "Year_comparison_cor");
+  // draw({extrema_combine_cor, extrema_combine_lin_cor}, {ellipse_combine_cor, ellipse_combine_lin_cor}, {kBlue+2, kRed+2}, {"with quadratic", "only linear"}, "linear_comparison");
+  // draw({extrema_combine, extrema_combine_cor}, {ellipse_combine, ellipse_combine_cor}, {kBlue+2, kRed+2}, {"uncorrelated", "correlated"}, "correlation_comparison");
+  // draw({extrema_combine, extrema_muon, extrema_elec}, {ellipse_combine, ellipse_muon, ellipse_elec}, {kBlue+2, kRed+2, kGreen+2}, {"combine", "muon", "elec"}, "channel_comparison");
+  // draw({extrema_combine, extrema_add_comb}, {ellipse_combine, ellipse_add_comb}, {kBlue+2, kRed+2}, {"data-bkg", "tt+bkg"}, "bkg_comparison");
+  // draw({extrema_combine, extrema_V20_comb}, {ellipse_combine, ellipse_V20_comb}, {kBlue+2, kRed+2}, {"JEC V11", "JEC V20"}, "JECversion_comparison");
+  // draw({extrema_add_comb, extrema_add_muon, extrema_add_elec}, {ellipse_add_comb, ellipse_add_muon, ellipse_add_elec}, {kBlue+2, kRed+2, kGreen+2}, {"combine", "muon", "elec"}, "AddBKGChannelComparison");
+
+  CompareAreas({ellipse_combine_lin_cor, ellipse_2016_comb_cor, ellipse_2017_comb_cor, ellipse_2018_comb_cor}, {"Combine", "2016", "2017", "2018"});
+
+}
+
+// ======================================================================================================
+// ===  Compare Areas                                                                                 ===
+// ======================================================================================================
+
+void CompareAreas(vector<TEllipse*> ellipse, vector<TString> names){
+  if(ellipse.size()!=names.size()) throw runtime_error("ERROR in CompareAreas; Not the same size.");
+  vector<double> areas;
+  for(unsigned int i=0; i<ellipse.size();i++){
+    double area = ellipse[i]->GetR1()*ellipse[i]->GetR2()*TMath::Pi();
+    areas.push_back(area);
+    cout << "Area for "+names[i]+" is\tA = " << area << endl;
+  }
+  cout << names[0]+"/"+names[1] << "\t area " << 1/(areas[0]/areas[1]) << "\t lumi " << lumis[0]/lumis[1] << endl;
+  cout << names[0]+"/"+names[2] << "\t area " << 1/(areas[0]/areas[2]) << "\t lumi " << lumis[0]/lumis[2] << endl;
+  cout << names[0]+"/"+names[3] << "\t area " << 1/(areas[0]/areas[3]) << "\t lumi " << lumis[0]/lumis[3] << endl;
+  cout << names[1]+"/"+names[2] << "\t area " << 1/(areas[1]/areas[2]) << "\t lumi " << lumis[1]/lumis[2] << endl;
+  cout << names[2]+"/"+names[3] << "\t area " << 1/(areas[2]/areas[3]) << "\t lumi " << lumis[2]/lumis[3] << endl;
+  cout << names[1]+"/"+names[3] << "\t area " << 1/(areas[1]/areas[3]) << "\t lumi " << lumis[1]/lumis[3] << endl;
+
 }
 
 
@@ -96,8 +166,7 @@ int main(int argc, char* argv[]){
 // ======================================================================================================
 
 MapVD ExtractFromFile(TString path, TString c){
-  if(debug) cout << "\t\t ... get file" << endl;
-  cout << path << endl;
+  if(debug) cout << "\t\t ... get file " << path << endl;
   ifstream file(path);
   TString error = path+" Does NOT exists!";
   if(!file.good()) throw runtime_error(error);
@@ -138,7 +207,7 @@ MapVD ExtractFromFile(TString path, TString c){
   return JMSfactor;
 }
 
-inline VecD ExtractPoints(TString line, const TString jms){
+VecD ExtractPoints(TString line, const TString jms){
   line.ReplaceAll(jms, ""); // jms_(_x,_y)
   line.ReplaceAll(" ", ""); // _(_x,_y)
   line.ReplaceAll("(", ""); // (x,y)
@@ -187,7 +256,7 @@ inline double angle_to_xaxis(const VecD vec){
 // ======================================================================================================
 
 // ------------------------------------------------------------------------------------------------
-inline TGraph* graph_multiple_points(MapVD points){
+TGraph* graph_multiple_points(MapVD points){
   TGraph *graph = new TGraph();
   VecD nom = points["nom"];
   graph->SetPoint(0, points["nom"][0], points["nom"][1]);
@@ -199,7 +268,7 @@ inline TGraph* graph_multiple_points(MapVD points){
 }
 
 // ------------------------------------------------------------------------------------------------
-inline TEllipse* build_ellipse(MapVD points){
+TEllipse* build_ellipse(MapVD points, TString name){
 
   double distance_min_1 = distance_points(points["nom"], points["uu"]);
   double distance_min_2 = distance_points(points["nom"], points["dd"]);
@@ -239,7 +308,7 @@ inline void set_ellipse(TEllipse *ellipse, int color){
 }
 
 // ------------------------------------------------------------------------------------------------
-inline void draw(vector<TGraph*> graph, vector<TEllipse*> ellipse, vector<int> color,  vector<TString> names, TString name){
+void draw(vector<TGraph*> graph, vector<TEllipse*> ellipse, vector<int> color,  vector<TString> names, TString name){
   TString save_path = get_save_path();
 
   for(unsigned int i=0; i<color.size(); i++){
