c7e9f7d4561122d7dbac0bb571f0e91875ea1d0b
apaasch <alexanderpaa94@gmail.com>
2022-12-20 11:00:40 +0100

    Adjust config for SYS

--- config/syst_resubmit.sh
+++ config/syst_resubmit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ROOT_DIR="$(pwd)"
 
-for i in SFrameUncerts_$1/*/.
+for i in SFrameUncerts_$1/*
 do
     echo "Working on $ROOT_DIR/$i/conf.txt"
     cd "$ROOT_DIR/$i/"
-    sframe_batch.py -r conf.xml
+    sframe_batch.py -r config.xml
 done
 cd "$ROOT_DIR"
 
