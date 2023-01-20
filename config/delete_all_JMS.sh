066f822627d0154b5afcd906504683dd00f79ebe
apaasch <alexanderpaa94@gmail.com>
2023-01-20 13:09:43 +0100

    rm config scripts and xmls

--- config/delete_all_JMS.sh
+++
@@ -1,4 +0,0 @@
-delete_workdir_syst muon
-delete_workdir_syst elec
-
-rm -r SFrameUncerts_*
--- config/syst_merge_all.sh
+++
@@ -1,9 +0,0 @@
-source syst_merge.sh 2016MTopJetmuon
-source syst_merge.sh 2016MTopJetelec
-
-source syst_merge.sh 2017MTopJetmuon
-source syst_merge.sh 2017MTopJetelec
-# source syst_merge.sh 2017MTopJetelec
-
-source syst_merge.sh 2018MTopJetmuon
-source syst_merge.sh 2018MTopJetelec
